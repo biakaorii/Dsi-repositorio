d27665b6823a250415dc009ae70b58de08748647
brunoaudricc <brunoaso2005@gmail.com>
2025-09-15 17:10:06 -0300

    Estilizando LoginScreen

--- projeto/app/app.js
+++
@@ -1,6 +0,0 @@
-// filepath: c:\Users\rhuan\OneDrive\Documentos\workspace\dsi-repository\projeto\App.js
-import LoginScreen from './LoginScreen';
-
-export default function App() {
-  return <LoginScreen />;
-}
