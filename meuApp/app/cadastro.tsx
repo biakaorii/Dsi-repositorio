3845da83108253389cec19d0898f978f52d20963
Guilherme Barros <156708304+Onyx1912@users.noreply.github.com>
2025-09-16 20:54:20 -0300

    envio do código da tela de cadastro

---
+++ meuApp/app/cadastro.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function RegisterScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const router = useRouter();
+
+  const handleRegister = () => {
+    console.log("Nome:", name);
+    console.log("Email:", email);
+    console.log("Senha:", password);
+    router.push("/home");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Criar Conta</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Confirmar senha"
+        value={confirm}
+        onChangeText={setConfirm}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => router.back()}>
+        <Text style={styles.link}>Já tem uma conta? Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    padding: 20, 
+    backgroundColor: "#fbfbf9f9" 
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#2E8B57",
+  },
+  input: {
+    width: 312,
+    height: 45,
+    alignSelf: 'center',
+    backgroundColor: "#EDF5C4",
+    borderRadius: 15,
+    paddingHorizontal: 15,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+  },
+  button: {
+    width: 312,
+    height: 45,
+    alignSelf: 'center',
+    backgroundColor: "#2E8B57",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonText: { 
+    color: "#fff", 
+    fontSize: 18, 
+    fontWeight: "bold",
+  },
+  link: {
+    textAlign: "center",
+    color: "#03168F",
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+  },
+});
