6951fb0d07c251aa2af590a13798a739cee9504d
brunoaudricc <brunoaso2005@gmail.com>
2025-09-22 16:04:58 -0300

    Criacao de Search/Reajuste redefinir senha

---
+++ meuApp/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="logirrn" />
+      <Stack.Screen name="home" />
+      <Stack.Screen name="cadastro" />
+      <Stack.Screen name="recuperar_senha" />
+      <Stack.Screen name="recuperar_senha_codigo" />
+      <Stack.Screen name="recuperar_senha_nova.tsx" />
+      <Stack.Screen name="usuario" />
+      <Stack.Screen name="search"/>
+    </Stack>
+  );
+}
+
---
+++ meuApp/app/recuperar_senha.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import {Link } from "expo-router";
+
+export default function RecuperarSenhaEmail() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/icon.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.title}>Recuperar senha</Text>
+      <Text style={styles.subtitle}>
+        Digite seu e-mail para receber o código de redefinição de senha
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="seu@email.com"
+        placeholderTextColor="#8a8a8a"
+      />
+    <Link href={'/recuperar_senha_codigo'} asChild>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Enviar código</Text>
+      </TouchableOpacity>
+    </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: 'center', alignItems: 'center',
+    backgroundColor: '#fff', padding: 28,
+  },
+  logo: { width: 120, height: 120, marginBottom: 40 },
+  title: { fontSize: 20, fontWeight: 'bold', marginBottom: 8 },
+  subtitle: { fontSize: 14, color: '#555', textAlign: 'center', marginBottom: 20 },
+  input: {
+    width: 312,
+    height: 45,
+    alignSelf: 'center',
+    backgroundColor: "#EDF5C4",
+    borderRadius: 15,
+    paddingHorizontal: 15,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+  },
+  button: {
+    width: 312,
+    height: 45,
+    alignSelf: 'center',
+    backgroundColor: "#2E8B57",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+    buttonText: { 
+    color: "#fff", 
+    fontSize: 18, 
+    fontWeight: "bold",
+  },
+});
