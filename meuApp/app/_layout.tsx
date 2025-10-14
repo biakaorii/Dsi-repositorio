97d853e8af6cb7dc6386e26060f00be1a4dee5eb
Luizrattcaso <rattacasoluiz03@gmail.com>
2025-10-13 21:06:48 -0300

    Retirada de Alerts e implementação do Toast

--- meuApp/app/_layout.tsx
+++ meuApp/app/_layout.tsx
@@ -1,16 +1,22 @@
 import { Stack } from "expo-router";
+import Toast from 'react-native-toast-message';
+import { Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="home" />
-      <Stack.Screen name="cadastro" />
-      <Stack.Screen name="recuperar_senha" />
-      <Stack.Screen name="usuario" />
-      <Stack.Screen name="search"/>
-    </Stack>
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="home" />
+        <Stack.Screen name="cadastro" />
+        <Stack.Screen name="recuperar_senha" />
+        <Stack.Screen name="usuario" />
+        <Stack.Screen name="search"/>
+      </Stack>
+      <Toast topOffset={height / 2 - 60} />
+    </>
   );
 }
-
