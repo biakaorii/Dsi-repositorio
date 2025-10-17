186e2015968f70f9b32592478aed8df55e8bb55b
Rhuanvss <rhuanvictor1323@gmail.com>
2025-10-16 21:48:33 -0300

    adicionado o banco de dados firebase e cadastro e login de usuario usando o banco de dados

--- meuApp/app/_layout.tsx
+++ meuApp/app/_layout.tsx
@@ -1,25 +1,33 @@
+// app/_layout.tsx
 import { Stack } from "expo-router";
 import Toast from 'react-native-toast-message';
 import { Dimensions } from 'react-native';
+import { AuthProvider } from '../contexts/AuthContext';
+import { ReviewsProvider } from '../contexts/ReviewsContext';
 
 const { height } = Dimensions.get('window');
 
 export default function Layout() {
   return (
-    <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="login" />
-        <Stack.Screen name="home" />
-        <Stack.Screen name="cadastro" />
-        <Stack.Screen name="recuperar_senha" />
-        <Stack.Screen name="usuario" />
-        <Stack.Screen name="search"/>
-        <Stack.Screen name="select-profile"/>
-        <Stack.Screen name="reader-form"/>
-        <Stack.Screen name="entrepreneur-form"/>
-      </Stack>
-      <Toast topOffset={height / 2 - 60} />
-    </>
+    <AuthProvider>
+      <ReviewsProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="login" />
+          <Stack.Screen name="home" />
+          <Stack.Screen name="cadastro" />
+          <Stack.Screen name="recuperar_senha" />
+          <Stack.Screen name="usuario" />
+          <Stack.Screen name="search"/>
+          <Stack.Screen name="select-profile"/>
+          <Stack.Screen name="reader-form"/>
+          <Stack.Screen name="entrepreneur-form"/>
+          <Stack.Screen name="editarPerfil"/>
+          <Stack.Screen name="progresso"/>
+          <Stack.Screen name="book-details"/>
+        </Stack>
+        <Toast topOffset={height / 2 - 60} />
+      </ReviewsProvider>
+    </AuthProvider>
   );
 }
